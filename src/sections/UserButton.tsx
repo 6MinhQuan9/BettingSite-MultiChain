b2bf95ed224cbf29ff3dbe7c9c2712a179e78540
6MinhQuan9 <whitemice2012@gmail.com>
2024-09-15 16:27:48 +0700

    entry test: add button to connect metamask and trust wallet

--- src/sections/UserButton.tsx
+++ src/sections/UserButton.tsx
@@ -2,9 +2,10 @@ import { useWallet } from '@solana/wallet-adapter-react'
 import { useWalletModal } from '@solana/wallet-adapter-react-ui'
 import { useWalletAddress } from 'gamba-react-v2'
 import { GambaUi } from 'gamba-react-ui-v2'
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from '../components/Modal'
 import { truncateString } from '../utils'
+import { ethers } from "ethers";
 
 function ConnectedButton() {
   const [modal, setModal] = React.useState(false)
@@ -28,7 +29,7 @@ function ConnectedButton() {
         <GambaUi.Button
           onClick={() => setModal(true)}
         >
-          <div style={{display: 'flex', gap: '.5em', alignItems: 'center'}}>
+          <div style={{ display: 'flex', gap: '.5em', alignItems: 'center' }}>
             <img src={wallet.wallet?.adapter.icon} height="20px" />
             {truncateString(address.toBase58(), 3)}
           </div>
@@ -50,6 +51,75 @@ export function UserButton() {
     }
   }
 
+  const [data, setdata] = useState<any>({
+    address: "",
+    Balance: null,
+  });
+
+  const [account, setAccount] = useState(null);
+  const [walletConnected, setWalletConnected] = useState(false);
+
+  // Button handler button for handling a
+  // request event for metamask
+  const btnhandler = () => {
+    // Asking if metamask is already present or not
+    if (window.ethereum) {
+      // res[0] for fetching a first wallet
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res: any) =>
+          accountChangeHandler(res[0])
+        );
+    } else {
+      alert("install metamask extension!!");
+    }
+  };
+
+  // getbalance function for getting a balance in
+  // a right format with help of ethers
+  const getbalance = (address: any) => {
+    // Requesting balance method
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [address, "latest"],
+      })
+      .then((balance: any) => {
+        // Setting balance
+        setdata({
+          Balance:
+            ethers.utils.formatEther(balance),
+        });
+      });
+  };
+
+  // Function for getting handling all events
+  const accountChangeHandler = (account: any) => {
+    // Setting an address data
+    setdata({
+      address: account,
+    });
+
+    // Setting a balance
+    getbalance(account);
+  };
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        // Request account access
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+        setWalletConnected(true);
+        console.log('Trust Wallet connected:', accounts[0]);
+      } catch (error) {
+        console.error('User rejected the request:', error);
+      }
+    } else {
+      alert('Trust Wallet not found. Please install it or open this site in the Trust Wallet browser.');
+    }
+  };
+
   return (
     <>
       {wallet.connected ? <ConnectedButton /> : (
@@ -57,6 +127,34 @@ export function UserButton() {
           {wallet.connecting ? 'Connecting' : 'Connect'}
         </GambaUi.Button>
       )}
+
+      {!walletConnected && (
+        <div>
+          {data.address ? (
+            <div>
+              <h2>Wallet Connected</h2>
+              <p>Account: {data.address}</p>
+            </div>
+          ) : (
+            <button onClick={btnhandler}>Connect to metamask wallet</button>
+          )}
+        </div>
+      )}
+
+
+      {!data.address && (
+        <div>
+          {walletConnected ? (
+            <div>
+              <h2>Wallet Connected</h2>
+              <p>Account: {account}</p>
+            </div>
+          ) : (
+            <button onClick={connectWallet}>Connect Trust Wallet</button>
+          )}
+        </div>
+      )}
+
     </>
   )
 }
